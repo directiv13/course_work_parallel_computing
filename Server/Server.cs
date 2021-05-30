c8eff9bd749a027fdbc2ea701697b8f4e901ddbb
directiv13 <vladpoleshchuk@gmail.com>
2021-05-30 20:31:36 +0300

    feat: Server constructor created

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
+using System.Net;
 
 namespace Server
 {
     class Server
     {
-
+        private readonly int port = 8005;
+        private readonly Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private readonly IPEndPoint ipEndPoint;
+        public Server()
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+        }
     }
 }
