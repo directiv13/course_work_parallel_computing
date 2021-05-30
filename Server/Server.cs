a086ee58824f98f3e73de51c2479c7eadf2fa7e8
directiv13 <vladpoleshchuk@gmail.com>
2021-05-30 20:34:18 +0300

    refactor: Server constructor changes

--- Server/Server.cs
+++ Server/Server.cs
@@ -2,17 +2,25 @@
 using System.Net.Sockets;
 using System.Text;
 using System.Net;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace Server
 {
     class Server
     {
         private readonly int port = 8005;
-        private readonly Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private readonly Socket appSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         private readonly IPEndPoint ipEndPoint;
+
+        private readonly Dictionary<string, HashSet<string>> index;
         public Server()
         {
             ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            string jsonString = File.ReadAllText("InvertedIndex.json");
+            index = JsonSerializer.Deserialize<Dictionary<string, HashSet<string>>>(jsonString);
         }
+        
     }
 }
