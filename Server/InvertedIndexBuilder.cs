c2e04179b3881fdc46ba4ec758dd8ea51aa297b1
directiv13 <vladpoleshchuk@gmail.com>
2021-05-30 20:43:07 +0300

    refactor: InvertedIndex renamed to InvertedIndexBuilder

---
+++ Server/InvertedIndexBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class InvertedIndexBuilder
+    {
+        SortedDictionary<string, HashSet<string>> InvertedIndex { get; set; }
+        public int StartFileIndex { get; set; }
+        public int EndFileIndex { get; set; }
+
+        public InvertedIndexBuilder(int startFileIndex, int endFileIndex)
+        {
+            StartFileIndex = startFileIndex;
+            EndFileIndex = endFileIndex;
+            InvertedIndex = new SortedDictionary<string, HashSet<string>>();
+        }
+    }
+}
